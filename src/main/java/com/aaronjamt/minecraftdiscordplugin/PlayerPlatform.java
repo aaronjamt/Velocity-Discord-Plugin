e35c25b8ada756568dfda0136dea3ea63d099ad0
Aaronjamt <13600347+aaronjamt@users.noreply.github.com>
2024-07-20 01:35:35 -0700

    Add player version to platform info

--- src/main/java/com/aaronjamt/minecraftdiscordplugin/PlayerPlatform.java
+++ src/main/java/com/aaronjamt/minecraftdiscordplugin/PlayerPlatform.java
@@ -5,6 +5,7 @@ import net.lax1dude.eaglercraft.v1_8.plugin.gateway_velocity.EaglerXVelocity;
 import net.lax1dude.eaglercraft.v1_8.plugin.gateway_velocity.api.EaglerXVelocityAPIHelper;
 import org.geysermc.api.util.BedrockPlatform;
 import org.geysermc.floodgate.api.FloodgateApi;
+import org.geysermc.floodgate.api.player.FloodgatePlayer;
 import org.geysermc.floodgate.util.DeviceOs;
 import org.geysermc.geyser.api.GeyserApi;
 import org.geysermc.geyser.api.connection.GeyserConnection;
@@ -44,48 +45,7 @@ public class PlayerPlatform {
     public Platform getPlayerPlatform(Player player) {
         UUID playerID = player.getUniqueId();
 
-        if (hasGeyser) {
-            if (GeyserApi.api().connectionByUuid(playerID) != null) {
-                logger.debug("Player {} is a Geyser player!", playerID);
-                GeyserConnection connection = GeyserApi.api().connectionByUuid(playerID);
-                if (connection != null) {
-                    BedrockPlatform platform = connection.platform();
-                    if (platform == BedrockPlatform.UNKNOWN) {
-                        logger.warn("Player {} is a Geyser player, but was unable to find exact player platform.", playerID);
-                        return Platform.BEDROCK_GENERIC;
-                    } else
-                        return Platform.PlatformByBedrockName(platform.toString());
-                }
-            } else logger.debug("Player {} is not a Geyser player.", playerID);
-        } else logger.debug("Not checking whether player {} is a Geyser player: Geyser not installed!", playerID);
-
-        if (hasFloodgate) {
-            if (FloodgateApi.getInstance().isFloodgatePlayer(playerID)) {
-                DeviceOs platform = FloodgateApi.getInstance().getPlayer(playerID).getDeviceOs();
-                logger.debug("Player {} is a Floodgate player!", playerID);
-
-                if (platform == DeviceOs.UNKNOWN) {
-                    logger.warn("Player {} is a Floodgate player, but was unable to find exact player platform.", playerID);
-                    return Platform.BEDROCK_GENERIC;
-                } else
-                    return Platform.PlatformByBedrockName(platform.toString());
-            } else logger.debug("Player {} is not a Floodgate player.", playerID);
-        } else logger.debug("Not checking whether player {} is a Floodgate player: Floodgate not installed!", playerID);
-
-        if (hasEaglerCraft) {
-            if (EaglerXVelocityAPIHelper.getEaglerHandle(player) != null) {
-                return Platform.EAGLER;
-                logger.debug("Player {} is an EaglerCraft player!", playerID);
-            } else logger.debug("Player {} is not a EaglerCraft player.", playerID);
-        } else logger.debug("Not checking whether player {} is a EaglerCraft player: EaglerCraft not installed!", playerID);
-
-        // If the player isn't any of the above, assume they're on Java
-        // Add client version and branding as well, since we can access that information
-        // (EaglerCraft is stuck on 1.8.8 and Geyser always uses the latest protocol, so
-        // neither is very useful, and Geyser/Floodgate isn't necessarily accurate)
-        Platform result = Platform.JAVA_OR_UNKNOWN;
-
-        // Get Minecraft version
+        // Get Minecraft version (used for Java and EaglerCraft players)
         String minVersion = player.getProtocolVersion().getVersionIntroducedIn();
         String maxVersion = player.getProtocolVersion().getMostRecentSupportedVersion();
         String version;
@@ -97,14 +57,54 @@ public class PlayerPlatform {
             version = String.format("%s - %s", minVersion, maxVersion);
         }
 
-        // Get client brand (i.e. vanilla, forge, fabric)
-        String clientBrand = player.getClientBrand();
-        if (clientBrand == null) clientBrand = "";
-        else clientBrand = String.format("(%s)", clientBrand);
+        if (hasGeyser) {
+            if (GeyserApi.api().connectionByUuid(playerID) != null) {
+                logger.debug("Player {} is a Geyser player!", playerID);
+                GeyserConnection connection = GeyserApi.api().connectionByUuid(playerID);
+                if (connection != null) {
+                    BedrockPlatform platform = connection.platform();
+                    if (platform == BedrockPlatform.UNKNOWN) {
+                        logger.warn("Player {} is a Geyser player, but was unable to find exact player platform.", playerID);
+                        return Platform.BEDROCK_GENERIC;
+                    } else
+                        return Platform.PlatformByBedrockName(platform.toString()).setDescriptors(connection.version());
+                }
+            } else logger.debug("Player {} is not a Geyser player.", playerID);
+        } else logger.debug("Not checking whether player {} is a Geyser player: Geyser not installed!", playerID);
 
-        // Update the display name and return
-        result.displayName = String.format("Java %s %s", version, clientBrand);
-        return result;
+        if (hasFloodgate) {
+            if (FloodgateApi.getInstance().isFloodgatePlayer(playerID)) {
+                logger.debug("Player {} is a Floodgate player!", playerID);
+                FloodgatePlayer floodgatePlayer = FloodgateApi.getInstance().getPlayer(playerID);
+                DeviceOs platform = floodgatePlayer.getDeviceOs();
+
+                if (platform == DeviceOs.UNKNOWN) {
+                    logger.warn("Player {} is a Floodgate player, but was unable to find exact player platform.", playerID);
+                    return Platform.BEDROCK_GENERIC;
+                } else
+                    return Platform.PlatformByBedrockName(platform.toString()).setDescriptors(floodgatePlayer.getVersion());
+            } else logger.debug("Player {} is not a Floodgate player.", playerID);
+        } else logger.debug("Not checking whether player {} is a Floodgate player: Floodgate not installed!", playerID);
+
+        if (hasEaglerCraft) {
+            if (EaglerXVelocityAPIHelper.getEaglerHandle(player) != null) {
+                logger.debug("Player {} is an EaglerCraft player!", playerID);
+                return Platform.EAGLER.setDescriptors(version);
+            } else logger.debug("Player {} is not a EaglerCraft player.", playerID);
+        } else logger.debug("Not checking whether player {} is a EaglerCraft player: EaglerCraft not installed!", playerID);
+
+        // If the player isn't any of the above, assume they're on Java
+        Platform platform = Platform.JAVA_OR_UNKNOWN;
+
+        // Add any client branding
+        String clientBrand = player.getClientBrand();
+        if (clientBrand != null) {
+            clientBrand = String.format("(%s)", clientBrand);
+        } else {
+            clientBrand = "";
+        }
+
+        return platform.setDescriptors(version, clientBrand);
     }
 
     // TODO: These emojis don't seem to render properly, at least in my limited testing of "trying it once and giving up". Need to fix!
@@ -152,12 +152,14 @@ public class PlayerPlatform {
         private static final Platform[] ALL_PLATFORMS = values();
 
         private final String bedrockName;
-        private String displayName;
+        private final String humanReadableName;
         private final String iconName;
 
-        Platform(String bedrockName, String displayName, String iconName) {
+        private String displayName;
+
+        Platform(String bedrockName, String humanReadableName, String iconName) {
             this.bedrockName = bedrockName;
-            this.displayName = displayName == null ? bedrockName : displayName;
+            this.humanReadableName = humanReadableName == null ? bedrockName : humanReadableName;
             this.iconName = iconName;
         }
 
@@ -168,13 +170,18 @@ public class PlayerPlatform {
             return BEDROCK_GENERIC;
         }
 
+        public Platform setDescriptors(String... descriptors) {
+            displayName = humanReadableName + " " + String.join(" ", descriptors);
+            return this; // For chaining
+        }
+
         public String getIconPath() {
             return String.format("/platform-icons/%s.png", iconName);
         }
 
         @Override
         public String toString() {
-            return displayName;
+            return displayName == null ? humanReadableName : displayName;
         }
     }
 }
