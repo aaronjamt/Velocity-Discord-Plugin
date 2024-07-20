2235d4e3d8c0ecb68c796c5b6599df81ca7d2f48
Aaronjamt <13600347+aaronjamt@users.noreply.github.com>
2024-07-20 01:49:33 -0700

    Add template Fabric plugin This is just the template for a Fabric mod, still need to fill it out

---
+++ src/main/java/com/aaronjamt/minecraftdiscordplugin/serverhelper/FabricPlugin.java
@@ -0,0 +1,15 @@
+package com.aaronjamt.minecraftdiscordplugin.serverhelper;
+
+import net.fabricmc.api.ModInitializer;
+
+/** Fabric entry point. */
+public class FabricPlugin implements ModInitializer {
+    public FabricPlugin() {
+        // TODO
+    }
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
